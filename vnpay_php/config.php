2e4d7bd1a706d35aa32ae65dab7138d164cfa51c
alicezuberg930 <tien23851@gmail.com>
2022-05-17 13:07:01 +0700

    update

--- vnpay_php/config.php
+++ vnpay_php/config.php
@@ -1,4 +1,9 @@
 <?php $vnp_TmnCode = "Y4U88XFK"; //Mã website tại VNPAY 
 $vnp_HashSecret = "DTHXNFNBUMNKFKQOZVHTXUXNUQUUXMTV"; //Chuỗi bí mật
 $vnp_Url = "http://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
-$vnp_Returnurl = "http://localhost/zippohub/vnpay_php/vnpay_return.php";
+$a = explode("/", $_SERVER["SCRIPT_FILENAME"]);
+$string = '';
+for ($i = 3; $i < sizeof($a) - 1; $i++) {
+    $string .= $a[$i] . "/";
+}
+$vnp_Returnurl = "http://localhost/" . $string . "vnpay_return.php";
